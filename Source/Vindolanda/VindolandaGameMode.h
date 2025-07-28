bc12e9aded6b0467d4df8153821a1ee79fc0ea40
WJConnors <37446973+WJConnors@users.noreply.github.com>
2025-07-28 14:51:28 +0100

    New skeleton gamemode and landscape height adjust

---
+++ Source/Vindolanda/VindolandaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TopDownGameModeBase.h"
+#include "VindolandaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VINDOLANDA_API AVindolandaGameMode : public ATopDownGameModeBase
+{
+	GENERATED_BODY()
+	
+};
