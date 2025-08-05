fd83d2c4dc2568945da44c1991b0c80f7703d91b
WJConnors <37446973+WJConnors@users.noreply.github.com>
2025-08-05 15:12:51 +0100

    Base archer tower class

---
+++ Source/Vindolanda/ArcherTower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseBuilding.h"
+#include "ArcherTower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VINDOLANDA_API AArcherTower : public ABaseBuilding
+{
+	GENERATED_BODY()
+	
+};
